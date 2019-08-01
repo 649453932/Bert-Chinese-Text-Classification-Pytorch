1fc389fb3ed9c67cb2723a07e6cb8a09250a0c18
huwenxing <hwx0@vip.qq.com>
2019-08-01 18:50:20 +0800

    增加bert+各种

---
+++ models/bert_DPCNN.py
@@ -0,0 +1,79 @@
+# coding: UTF-8
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# from pytorch_pretrained_bert import BertModel, BertTokenizer
+from pytorch_pretrained import BertModel, BertTokenizer
+
+
+class Config(object):
+
+    """配置参数"""
+    def __init__(self, dataset):
+        self.model_name = 'bert'
+        self.train_path = dataset + '/data/train.txt'                                # 训练集
+        self.dev_path = dataset + '/data/dev.txt'                                    # 验证集
+        self.test_path = dataset + '/data/test.txt'                                  # 测试集
+        self.class_list = [x.strip() for x in open(
+            dataset + '/data/class.txt').readlines()]                                # 类别名单
+        self.save_path = dataset + '/saved_dict/' + self.model_name + '.ckpt'        # 模型训练结果
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   # 设备
+
+        self.require_improvement = 1000                                 # 若超过1000batch效果还没提升，则提前结束训练
+        self.num_classes = len(self.class_list)                         # 类别数
+        self.num_epochs = 3                                             # epoch数
+        self.batch_size = 128                                           # mini-batch大小
+        self.pad_size = 32                                              # 每句话处理成的长度(短填长切)
+        self.learning_rate = 5e-5                                       # 学习率
+        self.bert_path = './bert_pretrain'
+        self.tokenizer = BertTokenizer.from_pretrained(self.bert_path)
+        self.hidden_size = 768
+        self.num_filters = 250                                          # 卷积核数量(channels数)
+
+
+class Model(nn.Module):
+
+    def __init__(self, config):
+        super(Model, self).__init__()
+        self.bert = BertModel.from_pretrained(config.bert_path)
+        for param in self.bert.parameters():
+            param.requires_grad = True
+        # self.fc = nn.Linear(config.hidden_size, config.num_classes)
+        self.conv_region = nn.Conv2d(1, config.num_filters, (3, config.hidden_size), stride=1)
+        self.conv = nn.Conv2d(config.num_filters, config.num_filters, (3, 1), stride=1)
+        self.max_pool = nn.MaxPool2d(kernel_size=(3, 1), stride=2)
+        self.padding1 = nn.ZeroPad2d((0, 0, 1, 1))  # top bottom
+        self.padding2 = nn.ZeroPad2d((0, 0, 0, 1))  # bottom
+        self.relu = nn.ReLU()
+        self.fc = nn.Linear(config.num_filters, config.num_classes)
+
+    def forward(self, x):
+        context = x[0]  # 输入的句子
+        mask = x[2]  # 对padding部分进行mask，和句子一个size，padding部分用0表示，如：[1, 1, 1, 1, 0, 0]
+        encoder_out, text_cls = self.bert(context, attention_mask=mask, output_all_encoded_layers=False)
+        x = encoder_out.unsqueeze(1)  # [batch_size, 1, seq_len, embed]
+        x = self.conv_region(x)  # [batch_size, 250, seq_len-3+1, 1]
+
+        x = self.padding1(x)  # [batch_size, 250, seq_len, 1]
+        x = self.relu(x)
+        x = self.conv(x)  # [batch_size, 250, seq_len-3+1, 1]
+        x = self.padding1(x)  # [batch_size, 250, seq_len, 1]
+        x = self.relu(x)
+        x = self.conv(x)  # [batch_size, 250, seq_len-3+1, 1]
+        while x.size()[2] > 2:
+            x = self._block(x)
+        x = x.squeeze()  # [batch_size, num_filters(250)]
+        x = self.fc(x)
+        return x
+
+    def _block(self, x):
+        x = self.padding2(x)
+        px = self.max_pool(x)
+        x = self.padding1(px)
+        x = F.relu(x)
+        x = self.conv(x)
+        x = self.padding1(x)
+        x = F.relu(x)
+        x = self.conv(x)
+        x = x + px  # short cut
+        return x
---
+++ models/bert_RNN.py
@@ -0,0 +1,56 @@
+# coding: UTF-8
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from pytorch_pretrained import BertModel, BertTokenizer
+
+
+class Config(object):
+
+    """配置参数"""
+    def __init__(self, dataset):
+        self.model_name = 'bert'
+        self.train_path = dataset + '/data/train.txt'                                # 训练集
+        self.dev_path = dataset + '/data/dev.txt'                                    # 验证集
+        self.test_path = dataset + '/data/test.txt'                                  # 测试集
+        self.class_list = [x.strip() for x in open(
+            dataset + '/data/class.txt').readlines()]                                # 类别名单
+        self.save_path = dataset + '/saved_dict/' + self.model_name + '.ckpt'        # 模型训练结果
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   # 设备
+
+        self.require_improvement = 1000                                 # 若超过1000batch效果还没提升，则提前结束训练
+        self.num_classes = len(self.class_list)                         # 类别数
+        self.num_epochs = 3                                             # epoch数
+        self.batch_size = 128                                           # mini-batch大小
+        self.pad_size = 32                                              # 每句话处理成的长度(短填长切)
+        self.learning_rate = 5e-5                                       # 学习率
+        self.bert_path = './bert_pretrain'
+        self.tokenizer = BertTokenizer.from_pretrained(self.bert_path)
+        self.hidden_size = 768
+        self.filter_sizes = (2, 3, 4)                                   # 卷积核尺寸
+        self.num_filters = 256                                          # 卷积核数量(channels数)
+        self.dropout = 0.1
+        self.rnn_hidden = 768
+        self.num_layers = 2
+
+
+class Model(nn.Module):
+
+    def __init__(self, config):
+        super(Model, self).__init__()
+        self.bert = BertModel.from_pretrained(config.bert_path)
+        for param in self.bert.parameters():
+            param.requires_grad = True
+        self.lstm = nn.LSTM(config.hidden_size, config.rnn_hidden, config.num_layers,
+                            bidirectional=True, batch_first=True, dropout=config.dropout)
+        self.dropout = nn.Dropout(config.dropout)
+        self.fc_rnn = nn.Linear(config.rnn_hidden * 2, config.num_classes)
+
+    def forward(self, x):
+        context = x[0]  # 输入的句子
+        mask = x[2]  # 对padding部分进行mask，和句子一个size，padding部分用0表示，如：[1, 1, 1, 1, 0, 0]
+        encoder_out, text_cls = self.bert(context, attention_mask=mask, output_all_encoded_layers=False)
+        out, _ = self.lstm(encoder_out)
+        out = self.dropout(out)
+        out = self.fc_rnn(out[:, -1, :])  # 句子最后时刻的 hidden state
+        return out
