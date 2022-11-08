1180212e9a7c34dcc9fc35c3c90b0536df0a35ff
guodongxiaren <879231132@qq.com>
2022-11-08 22:21:16 +0800

    x

--- models/bert.py
+++ models/bert.py
@@ -38,13 +38,9 @@ class Model(nn.Module):
             param.requires_grad = True
         self.fc = nn.Linear(config.hidden_size, config.num_classes)
 
-    def forward(self, x, y=None, z=None):
-        if z is None:
-            context = x[0]  # 输入的句子
-            mask = x[2]  # 对padding部分进行mask，和句子一个size，padding部分用0表示，如：[1, 1, 1, 1, 0, 0]
-        else:
-            context = x
-            mask = z
+    def forward(self, x):
+        context = x[0]  # 输入的句子
+        mask = x[2]  # 对padding部分进行mask，和句子一个size，padding部分用0表示，如：[1, 1, 1, 1, 0, 0]
         _, pooled = self.bert(context, attention_mask=mask, output_all_encoded_layers=False)
         out = self.fc(pooled)
         return out
