4c3a575a9ffef62b861a73028067cedda41dd933
guodongxiaren <879231132@qq.com>
2022-11-12 13:31:07 +0800

    diff

--- ort_pred.py
+++ ort_pred.py
@@ -2,22 +2,55 @@
 # coding=utf-8
 import onnxruntime as ort
 import numpy as np
-from pred import build_predict_text, key
-from pred import build_predict_text1
+from pred import key, config
+import pred
 import time
 
-def get_ort_session(model_path, providers = None):
-    # providers : ['TensorrtExecutionProvider', 'CUDAExecutionProvider', 'CPUExecutionProvider']
-    if providers is None:
-        providers = ort.get_available_providers()
-    return [ort.InferenceSession(model_path, providers=[provider]) for provider in providers]
-
 def to_numpy(tensor):
+    """
+    Tensor转numpy数组
+    """
     return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
 
-def predict(sess, text):
-    ids, seq_len, mask = build_predict_text(t)
-    print(type(ids))
+def get_ort_session(model_path, provider_prefix):
+    '''
+    获取指定provider的Session
+    provider_prefix : ['Tensorrt', 'CUDA', 'CPU']
+    '''
+    provider = provider_prefix + 'ExecutionProvider'
+    return ort.InferenceSession(model_path, providers=[provider])
+
+def get_all_ort_session(model_path):
+    # providers : ['TensorrtExecutionProvider', 'CUDAExecutionProvider', 'CPUExecutionProvider']
+    providers = ort.get_available_providers()
+    return [ort.InferenceSession(model_path, providers=[provider]) for provider in providers]
+
+def build_predict_text1(text):
+    """
+    不返回Tensor类型，直接返回list
+    """
+    token = config.tokenizer.tokenize(text)
+    token = ['[CLS]'] + token
+    seq_len = len(token)
+    mask = []
+    token_ids = config.tokenizer.convert_tokens_to_ids(token)
+    pad_size = config.pad_size
+    # 下面进行padding，用0补足位数
+    if pad_size:
+        if len(token) < pad_size:
+            mask = [1] * len(token_ids) + ([0] * (pad_size - len(token)))
+            token_ids += ([0] * (pad_size - len(token)))
+        else:
+            mask = [1] * pad_size
+            token_ids = token_ids[:pad_size]
+            seq_len = pad_size
+    return [token_ids], [seq_len], [mask]
+
+def predict0(sess, text):
+    """
+    输入采用torch.Tensor转numpy数组
+    """
+    ids, seq_len, mask = pred.build_predict_text(text)
 
     input = {
         sess.get_inputs()[0].name: to_numpy(ids),
@@ -27,71 +60,54 @@ def predict(sess, text):
     num = np.argmax(outs)
     return key[num]
 
-def predict1(sess, text):
-    ids, seq_len, mask = build_predict_text1(t)
+def predict(sess, text):
+    ids, seq_len, mask = build_predict_text1(text)
 
     input = {
         sess.get_inputs()[0].name: np.array(ids),
         sess.get_inputs()[1].name: np.array(mask),
     }
-    outs = sess.run(None, input)
-    num = np.argmax(outs)
-    return key[num]
-
-def predict2(sess, text):
-    ids, seq_len, mask = build_predict_text1(t)
-
+    """
+    等价于：
     input = {
         'ids': np.array(ids),
         'mask': np.array(mask),
     }
+    """
     outs = sess.run(None, input)
     num = np.argmax(outs)
     return key[num]
 
+def load_title(fname):
+    ts = []
+    with open(fname)  as f:
+        for line in f.readlines():
+            ts.append(line.strip())
+    return ts
+
+def batch_predict(ts, predict_fun, name):
+    print('')
+    a = time.time()
+    for t in ts:
+        res = predict_fun(t)
+        print('%s is %s' % (t, res))
+    b = time.time()
+    print('%s cost: %.4f' % (name, (b - a)))
+
 if __name__ == '__main__':
-    #sesses = get_ort_session("./model.onnx", ['CUDAExecutionProvider', 'CPUExecutionProvider'])
-    sesses = get_ort_session("./model.onnx", ['CUDAExecutionProvider'])
+    model_path = './model.onnx'
+    #sesses = get_all_ort_session('./model.onnx')
+    cuda_ses = get_ort_session(model_path, 'CUDA')
 
-    '''
-    sess = sesses[0]
-    print(len(sess.get_inputs()))
-    for i in sess.get_inputs():
-        print(i.name)
-    exit("")
-    '''
     ts = [
-        "李稻葵:过去2年抗疫为每人增寿10天",
-        "4个小学生离家出走30公里想去广州塔",
-        "朱一龙戏路打通电影电视剧",
-        "天问一号着陆火星一周年",
+        '兰州野生动物园观光车侧翻事故新进展：2人经抢救无效死亡',
+        '4个小学生离家出走30公里想去广州塔',
+        '朱一龙戏路打通电影电视剧',
+        '天问一号着陆火星一周年',
+
     ]
-    for sess in sesses: 
-        print("\n")
-        a = time.time()
-        for t in ts:
-            res = predict(sess, t)
-            print("%s is %s" % (t, res))
-        b = time.time()
-        provider = sess._providers[0]
-        print("%s cost: %.4f" % (provider, (b - a)))
+    ts = load_title('./news_title.txt')
+    #batch_predict(ts, lambda t: predict0(cuda_ses, t), 'ONNX_CUDA_tensor_to_numpy')
+    batch_predict(ts, lambda t: predict(cuda_ses, t), 'ONNX_CUDA')
+    batch_predict(ts, lambda t: pred.predict(t), 'Pytorch_CUDA')
 
-    for sess in sesses: 
-        print("\n")
-        a = time.time()
-        for t in ts:
-            res = predict1(sess, t)
-            print("%s is %s" % (t, res))
-        b = time.time()
-        provider = sess._providers[0]
-        print("%s cost: %.4f" % (provider, (b - a)))
-
-    for sess in sesses: 
-        print("\n")
-        a = time.time()
-        for t in ts:
-            res = predict2(sess, t)
-            print("%s is %s" % (t, res))
-        b = time.time()
-        provider = sess._providers[0]
-        print("%s cost: %.4f" % (provider, (b - a)))
--- pred.py
+++ pred.py
@@ -21,24 +21,6 @@ config = x.Config('THUCNews')
 model = x.Model(config).to(config.device)
 model.load_state_dict(torch.load(config.save_path, map_location='cpu'))
 
-def build_predict_text1(text):
-    token = config.tokenizer.tokenize(text)
-    token = ['[CLS]'] + token
-    seq_len = len(token)
-    mask = []
-    token_ids = config.tokenizer.convert_tokens_to_ids(token)
-    pad_size = config.pad_size
-    # 下面进行padding，用0补足位数
-    if pad_size:
-        if len(token) < pad_size:
-            mask = [1] * len(token_ids) + ([0] * (pad_size - len(token)))
-            token_ids += ([0] * (pad_size - len(token)))
-        else:
-            mask = [1] * pad_size
-            token_ids = token_ids[:pad_size]
-            seq_len = pad_size
-    return [token_ids], [seq_len], [mask]
-
 def build_predict_text(text):
     token = config.tokenizer.tokenize(text)
     token = ['[CLS]'] + token
