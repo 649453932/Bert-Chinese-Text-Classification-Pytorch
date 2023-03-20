907b46ee3a8c1a28bc40bead9497a7bdee9904f8
ShawnXuan <xiexuanx2@gmail.com>
2023-03-20 20:55:26 +0800

    support oneflow run

---
+++ of_run.py
@@ -0,0 +1,41 @@
+import oneflow as flow
+
+flow.mock_torch.enable()
+
+# coding: UTF-8
+import time
+import torch
+import numpy as np
+from train_eval import train, init_network
+from importlib import import_module
+import argparse
+from utils import build_dataset, build_iterator, get_time_dif
+
+parser = argparse.ArgumentParser(description='Chinese Text Classification')
+parser.add_argument('--model', type=str, required=True, help='choose a model: Bert, ERNIE')
+args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    dataset = 'THUCNews'  # 数据集
+
+    model_name = args.model  # bert
+    x = import_module('models.' + model_name)
+    config = x.Config(dataset)
+    np.random.seed(1)
+    torch.manual_seed(1)
+    torch.cuda.manual_seed_all(1)
+    torch.backends.cudnn.deterministic = True  # 保证每次结果一样
+
+    start_time = time.time()
+    print("Loading data...")
+    train_data, dev_data, test_data = build_dataset(config)
+    train_iter = build_iterator(train_data, config)
+    dev_iter = build_iterator(dev_data, config)
+    test_iter = build_iterator(test_data, config)
+    time_dif = get_time_dif(start_time)
+    print("Time usage:", time_dif)
+
+    # train
+    model = x.Model(config).to(config.device)
+    train(config, model, train_iter, dev_iter, test_iter)
--- train_eval.py
+++ train_eval.py
@@ -36,16 +36,18 @@ def train(config, model, train_iter, dev_iter, test_iter):
     optimizer_grouped_parameters = [
         {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
         {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}]
-    # optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
-    optimizer = BertAdam(optimizer_grouped_parameters,
-                         lr=config.learning_rate,
-                         warmup=0.05,
-                         t_total=len(train_iter) * config.num_epochs)
+    #optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
+    # optimizer = BertAdam(optimizer_grouped_parameters,
+    #                      lr=config.learning_rate,
+    #                      warmup=0.05,
+    #                      t_total=len(train_iter) * config.num_epochs)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=config.learning_rate, weight_decay=0.01, betas=(0.9, 0.999), eps=1e-8)
     total_batch = 0  # 记录进行到多少batch
     dev_best_loss = float('inf')
     last_improve = 0  # 记录上次验证集loss下降的batch数
     flag = False  # 记录是否很久没有效果提升
     model.train()
+    time_start = time.time()
     for epoch in range(config.num_epochs):
         print('Epoch [{}/{}]'.format(epoch + 1, config.num_epochs))
         for i, (trains, labels) in enumerate(train_iter):
@@ -79,6 +81,8 @@ def train(config, model, train_iter, dev_iter, test_iter):
                 break
         if flag:
             break
+    print(f"training time = {time.time() - start_time}")
+
     test(config, model, test_iter)
 
 
